c66e30607167abfdf2b1354402b054ef69b69127
digkvs <diegokaviski14@gmail.com>
2025-10-08 16:52:00 -0300

    buzzer and pump adjust

--- Esp32/py.py
+++ Esp32/py.py
@@ -6,39 +6,62 @@ import ujson
 import dht
 import socket
 
-
-
+    # led1 = Irrigacao
+    # led2 = Ventilacao
+    # led3 = Door
+    # led4 = Luz
+    # led5 = Automatico
+    
 #leds
-led1 = Pin(14, Pin.OUT)  
-led2 = Pin(32, Pin.OUT)  
+    
+    
+led1 = Pin(14, Pin.OUT)
+rele2 = Pin(32, Pin.OUT)
 led3 = Pin(33, Pin.OUT)
 led4 = Pin(5, Pin.OUT)
 led5 = Pin(4, Pin.OUT)
+
 rele1 = Pin(12,Pin.OUT)
 #ultrasom
 TRIG = Pin(18, Pin.OUT)
 ECHO = Pin(19, Pin.IN)
 #buzzer
-buzz = Pin(13, Pin.OUT)
+buzzer_pin = Pin(13, Pin.OUT)
+buzzer = PWM(buzzer_pin)
 
-    notas = {
-    'B3': 247, 'C4': 261, 'C#4': 277, 'D4': 294, 'D#4': 311, 'E4': 330,
-    'F4': 349, 'F#4': 370, 'G4': 392, 'G#4': 415, 'A4': 440, 'A#4': 466,
-    'B4': 494, 'C5': 523, 'C#5': 554, 'D5': 587, 'D#5': 622, 'E5': 659,
-    'F5': 698, 'F#5': 740, 'G5': 784, 'G#5': 831, 'A5': 880, 'A#5': 932,
-    'B5': 988, 'C6': 1047
-}
+frequencia = 412
+
+notas = [
+    523,  
+    0,    
+    440,  
+    0,    
+    392,  
+    0,    
+    330,  
+    0,    
+    262,  
+    0     
+]
     
-def tocarmusica(freq,duracao):
-    if freq == 0: 
-        buzzer.duty(0)
-    else:
-        buzzer.freq(freq)
-        buzzer.duty(512)
-    time.sleep(duracao)
-    buzzer.duty(0)
-    time.sleep(0.02)
 
+duracoes = [
+    0.15, 0.1,  
+    0.15, 0.1,  
+    0.15, 0.1,  
+    0.15, 0.1,  
+    0.2, 0.5   
+]
+
+
+def tocar_nota(frequencia, duracao):
+    if frequencia > 0:
+        buzzer.freq(frequencia)
+        buzzer.duty(412)  
+    else:
+        buzzer.duty(0)  
+    time.sleep(duracao) 
+    buzzer.duty(0)
 
 def get_distance():
     # Dispara pulso
@@ -108,17 +131,17 @@ conectarWifi()
 
 
 while True:
-    
+    buzzer.duty(0)
     sensor_ldr = ADC(Pin(34))
     sensor_dht = dht.DHT11(Pin(25))
     sensor_dht_ext = dht.DHT11(Pin(26))
 
-    
+     
     sensor_ldr.atten(ADC.ATTN_11DB)
     sensor_ldr.width(ADC.WIDTH_12BIT)
     
         
-    #sensor_dht.measure()
+    sensor_dht.measure()
     sensor_dht_ext.measure()
     
     dht_h = sensor_dht.humidity() 
@@ -166,8 +189,9 @@ while True:
         led5.value(0)
         if dht_t >= 21:
             enviarFire(1, "Acionadores/Ventilacao")
-            led2.value(1)
+            rele2.value(1)
             rele1.value(1)
+            buzzer.duty(0)
             
     else:
         led5.value(1)
@@ -179,22 +203,36 @@ while True:
         print(irrigacao, ventilacao, luz, comporta)
         
         if irrigacao == 1:
+            
             led1.value(1)
-        if ventilacao == 1:
-            led2.value(1)
             rele1.value(1)
-        if luz == 1:
+            time.sleep_ms(500)
+            rele1.value(0)
+            for i in range(len(notas)):
+                tocar_nota(notas[i], duracoes[i])
+                time.sleep(0.05)
+            time.sleep(1)
+            
+        if ventilacao == 1:
+            rele2.value(1)            
             led3.value(1)
+            
+        # if luz == 1:
+            
         if comporta == 1:
             led4.value(1)
             
         if irrigacao == 0:
             led1.value(0)
-        if ventilacao == 0:
-            led2.value(0)
+            buzzer.duty(0)
             rele1.value(0)
-        if luz == 0:
+            
+        if ventilacao == 0:
+            rele2.value(0)
+            rele1.value(0)
             led3.value(0)
+        #if luz == 0:
+            
         if comporta == 0:
             led4.value(0)  
         
@@ -202,3 +240,6 @@ while True:
     enviarFire(data, "Sensores")
     time.sleep(1)
     
+
+
+
